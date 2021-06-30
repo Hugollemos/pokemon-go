bdc346884cf344bccc39360cef6be5a32400989f
Hugo Lemos Da Silva <hugolemosdasilva.hu@gmail.com>
2021-06-29 23:36:47 -0300

    update

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-image">
         <figure>
-          <img :src="pokemon.front" alt="Placeholder image" />
+          <img :src="currentImg" alt="Placeholder image" />
         </figure>
       </div>
       <div class="card-content">
@@ -12,7 +12,9 @@
           <p class="subtitle is-6">{{ pokemon.type }}</p>
         </div>
       </div>
-      <div class="content"></div>
+      <div class="content">
+        <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar Sprite</button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,11 +28,14 @@ export default {
       this.pokemon.type = res.data.types[0].type.name;
       this.pokemon.front = res.data.sprites.front_default;
       this.pokemon.back = res.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
       console.log(this.pokemon);
     });
   },
   data() {
     return {
+      isfront: true,
+      currentImg: '', 
       pokemon: {
         type: "",
         front: "",
@@ -49,6 +54,17 @@ export default {
       return newName;
     },
   },
+  methods: {
+    mudarSprite: function(){
+      if(this.isFront){
+        this.isFront = false;
+        this.currentImg =this.pokemon.back;
+      }else{
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+      }
+    }
+  }
 };
 </script>
   
