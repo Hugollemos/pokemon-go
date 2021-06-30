c8a5b5ba7484dd9d76841a337f867b9142ef789e
Hugo Lemos Da Silva <hugolemosdasilva.hu@gmail.com>
2021-06-30 12:05:53 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
-      <img src="./assets/pokemon.png"/>
-      <hr>
+      <img src="./assets/pokemon.png" />
+      <hr />
       <h4 class="is-size-3">Pokedex</h4>
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <input
+        type="text"
+        placeholder="Buscar pokemon por nomes"
+        v-model="busca"
+        class="input is-rounded"
+      />
+      <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">
+        Buscar
+      </button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
         <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
       </div>
     </div>
@@ -13,35 +22,50 @@
 
 <script>
 import axios from "axios";
-import Pokemon from "./components/Pokemon.vue";
+import Pokemon from "./components/Pokemon";
 export default {
   name: "App",
   data() {
     return {
       pokemons: [],
-    };
+      filteredPokemons: [],
+      busca: ''
+    }
   },
-  created: function () {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
-      .then((res) => {
-        console.log("Pegou a lista de pokemons");
-        this.pokemons = res.data.results;
-      });
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then((res) => {
+      console.log("Pegou a lista de pokemons");
+      this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
+    })
   },
-  components: {
-    Pokemon,
+  components:{
+    Pokemon
   },
-};
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+}
 </script>
 
 <style>
-#app {
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#buscaBtn{
+  margin-top: 2%;
 }
 </style>
